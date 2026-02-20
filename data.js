451ae21fb309b1c58f318ed0fcb35781c7241818
anushaadukondalu-boop <anushaadukondalu@gmail.com>
2026-02-20 12:13:47 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,89 @@
+/* ===============================
+   EDUSTREAMIX - B.Com Semester 4
+================================= */
+
+window.data = {
+
+"B.Com General - Semester 4": [
+
+/* ---------- LANGUAGES ---------- */
+
+{
+name:"English",
+channels:[
+{
+name:"English by Nisa",
+link:"https://www.youtube.com/playlist?list=PLUJXB7al70IAIbiajs7JqxrGqc-tgi80j"
+},
+{
+name:"Sara’s Learning Point",
+link:"https://www.youtube.com/playlist?list=PLjNdhMBtsxpz5BYkI4eiiYQahD7GroZy0"
+}
+]
+},
+
+{
+name:"Telugu",
+channels:[
+{
+name:"Dandu Venkatramulu",
+link:"https://www.youtube.com/playlist?list=PLyjVm5GfnmY5y3l69NvP0rfdM3PIr1YTn"
+}
+]
+},
+
+/* ---------- CORE SUBJECTS ---------- */
+
+{
+name:"Corporate Accounting",
+channels:[
+{
+name:"Hasham Ali Khan",
+link:"https://www.youtube.com/playlist?list=PLWJDzVuPkXAlXTMbitI-ZDjiAmi7M8Zww"
+}
+]
+},
+
+{
+name:"Business Statistics – 2",
+channels:[
+{
+name:"Hasham Ali Khan",
+link:"https://www.youtube.com/playlist?list=PLWJDzVuPkXAk3twapgb0DQ9MyaQ24V4JS"
+}
+]
+},
+
+{
+name:"Income Tax",
+channels:[
+{
+name:"Devika’s Commerce",
+link:"https://www.youtube.com/playlist?list=PLP6CJ8yrRpA3_WbjoudHcT10AxY3Tut1B"
+}
+]
+},
+
+{
+name:"Universal Human Values",
+channels:[
+{
+name:"Tegron Learning Hub",
+link:"https://www.youtube.com/watch?v=PZQCoQdLJmw"
+}
+]
+},
+
+{
+name:"Practice of Life and General Insurance",
+channels:[
+{
+name:"Dwivedi Guidance",
+link:"https://www.youtube.com/playlist?list=PLsh2FvSr3n7cD0F2FSjBVWWrVF6cR8Pla"
+}
+]
+}
+
+]
+
+};
